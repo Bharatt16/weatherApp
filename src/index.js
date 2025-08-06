440f898f3cdd7628ae2a422d542a42fcb7554ae7
ghostt <194278228+Ghostt-coder@users.noreply.github.com.>
2025-08-06 19:00:28 +0530

    used APIs and created js for retreiving data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import "./styles.css";
 
-console.log("Hello World");
+let baseURL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/location/date-1/date-2?key=MT84LCFDNK2XS7NYCV64E8J6V'
+
+let submitBtn = document.querySelector('.searchDiv>button');
+let input = document.querySelector('.searchDiv>input')
+
+submitBtn.addEventListener('click', async () =>{
+     let location = input.value.toLowerCase();
+     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=MT84LCFDNK2XS7NYCV64E8J6V`);
+     const data = await response.json();
+     console.log(data.days[0]);
+     let temp = data.days[0].temp;
+     let conditions = data.days[0].conditions;
+     let feelsLike = data.days[0].feelslike;
+     let humidity = data.days[0].humidity;
+     let wind = data.days[0].windspeed;
+     console.log(`temp : ${temp} , condition : ${conditions} , feelslike : ${feelsLike} , humdity : ${humidity} , wind : ${wind}`)
+})
+
+
